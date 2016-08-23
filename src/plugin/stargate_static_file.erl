239bda23bd63cf272fb6d06e057df279011a832c
vans163 <vans_163@yahoo.com>
2016-08-23 17:52:29 -0400

    fixups to plugin. little optimize for gzip

--- src/plugin/stargate_static_file.erl
+++ src/plugin/stargate_static_file.erl
@@ -1,6 +1,10 @@
 -module(stargate_static_file).
 -compile(export_all).
 
+-ifndef(PRINT).
+-define(PRINT(Var), io:format("~p:~p - ~p~n", [?MODULE, ?LINE, Var])).
+-endif.
+
 sanitize_forward_slash(Path) ->
     PathCleaner = binary:replace(Path, <<"//">>, <<"/">>),
     case binary:match(PathCleaner, <<"//">>) of
@@ -25,6 +29,17 @@ sanitize_path(Path) ->
     sanitize_forward_slash(_Fullpath)
 .
 
+can_accept_gzip(Headers) ->
+    case maps:get('Accept-Encoding', Headers, undefined) of
+        undefined -> no;
+        Bin ->
+            case binary:match(Bin, <<"gzip">>) of
+                nomatch -> no;
+                _ -> yes
+            end
+    end
+    .
+
 serve_static(Path, Headers, S) ->
     Fullpath = <<(sanitize_path(Path))/binary>>,
     case filelib:is_regular(Fullpath) of
@@ -33,15 +48,39 @@ serve_static(Path, Headers, S) ->
 
         true ->
             {ok, Bin} = file:read_file(Fullpath),
-            GZipBin = zlib:gzip(Bin),
 
             Etag = maps:get('If-None-Match', Headers, undefined),
-            Crc32 = erlang:integer_to_binary(erlang:crc32(GZipBin)),
-            if
-                Etag == Crc32 -> 
-                    {304, #{}, <<>>, S};
-                true -> 
-                    {200, #{<<"Content-Encoding">>=> <<"gzip">>, <<"Etag">>=> Crc32}, GZipBin, S}
-            end
+
+            CanGZip = can_accept_gzip(Headers),
+            {Code, HeadersReply, BinReply} = case filename:extension(Fullpath) of
+                <<".png">> -> serve_static_no_gzip(Bin, Etag);
+                <<".jpg">> -> serve_static_no_gzip(Bin, Etag);
+                _ when CanGZip == yes -> serve_static_gzip(Bin, Etag);
+                _ -> serve_static_no_gzip(Bin, Etag)
+            end,
+
+            {Code, HeadersReply, BinReply, S}
     end
     .
+
+serve_static_no_gzip(Bin, Etag) ->
+    Crc32 = erlang:integer_to_binary(erlang:crc32(Bin)),
+    if
+        Etag == Crc32 -> 
+            {304, #{}, <<>>};
+        true -> 
+            {200, #{<<"Etag">>=> Crc32}, Bin}
+    end.
+
+serve_static_gzip(Bin, Etag) ->
+    GZipBin = zlib:gzip(Bin),
+    Crc32 = erlang:integer_to_binary(erlang:crc32(GZipBin)),
+    if
+        Etag == Crc32 -> 
+            {304, #{}, <<>>};
+        true -> 
+            {200, #{<<"Content-Encoding">>=> <<"gzip">>, <<"Etag">>=> Crc32}, GZipBin}
+    end.
+
+
+

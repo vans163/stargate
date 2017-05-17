87371126739155031b947968624eb569a2c1ac73
vans163 <vans_163@yahoo.com>
2017-05-16 22:21:02 -0400

    fix sneaky host2

--- src/handler/stargate_handler_redirect_https.erl
+++ src/handler/stargate_handler_redirect_https.erl
@@ -2,6 +2,6 @@
 -export([http/6]).
 
 http(_Type, Path, _Query, HttpHeaders, _Body, SessionState) ->
-    Host = maps:get("host", HttpHeaders, <<"">>),
+    Host = maps:get(<<"host">>, HttpHeaders, <<"">>),
     SSLPath = <<"https://", Host/binary, Path/binary>>,
     {301, #{<<"Location">>=> SSLPath}, <<"">>, SessionState}.

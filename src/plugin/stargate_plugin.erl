a590b05031e31d8f77693a1b6dedf3e1ceb5b1f4
vans163 <vans_163@yahoo.com>
2017-09-11 12:23:19 -0400

    handle a non-key-value cookie

--- src/plugin/stargate_plugin.erl
+++ src/plugin/stargate_plugin.erl
@@ -8,11 +8,13 @@ template(Page, KeyValue) -> stargate_template:transform(Page, KeyValue).
 template(RE, Page, KeyValue) -> stargate_template:transform(RE, Page, KeyValue).
 
 %TODO: Does this conform/work in all cases?
-cookie_parse(CookiesBin) -> 
+cookie_parse(CookiesBin) when is_binary(CookiesBin) == false -> #{};
+cookie_parse(CookiesBin) ->
     CookiesSplit = binary:split(CookiesBin, <<"; ">>, [global]),
     lists:foldl(fun(CookieBin, Acc) ->
             case binary:split(CookieBin, <<"=">>) of
                 [K, V] -> Acc#{K=> V};
+                [K] -> Acc#{<<"_">>=> K};
                 _ -> Acc
             end
         end, #{}, CookiesSplit
